84979f5075b840cf22c787045e81aa65b6eb3e62
Guilherme <gleite98@gmail.com>
2024-09-15 00:09:49 -0300

    Estilizando página de projetos de e-commerce

--- src/pages/projetos/sites-ecommerce/index.tsx
+++ src/pages/projetos/sites-ecommerce/index.tsx
@@ -2,16 +2,18 @@ import { Dropdown } from '@mui/base/Dropdown';
 import { MenuButton } from '@mui/base/MenuButton';
 import { Menu } from '@mui/base/Menu';
 import { MenuItem } from '@mui/base/MenuItem';
-
 import NavbarDesktop from "@/components/NavbarDesktop";
 import { Box, Container, Typography } from "@mui/material";
 import Head from "next/head";
 import styles from './styles.module.scss';
 import { useEffect, useMemo, useRef, useState } from 'react';
-
 import { ecommerceProjects } from '@/providers/ItemsList';
 import Link from 'next/link';
 import Image from 'next/image';
+import { MuiTheme } from '@/providers/MuiColors';
+import { CircularProgress } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';   
+import { faChevronDown, faChevronRight, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 enum platforms{
     all,
@@ -22,13 +24,16 @@ enum platforms{
 
 const SitesEcommerce = () => {
     const [articles, setArticles] = useState<any[]>([]);
+    const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+    const [selected, setSelected] = useState<platforms>(platforms.all);
     const galleryElement = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         if(galleryElement.current){
             galleryElement.current.querySelectorAll(`.${styles.siteImage}`).forEach(img => {
                 img.addEventListener("load", () => {
-                    img.classList.add(styles.loaded);
+                    img.classList.add("loaded");
+                    img.closest(`.${styles.platform}`)?.querySelector('span[role="progressbar"]')?.remove();
                     console.log("Imagem carregada com sucesso:");
                     console.log(img);
                 });
@@ -36,7 +41,6 @@ const SitesEcommerce = () => {
                     console.log("Erro ao carregar a imagem:");
                     console.log(img);
                 });
-                
             })
         }
     }, [articles])
@@ -69,7 +73,7 @@ const SitesEcommerce = () => {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main className="page flex">
+            <main className="page flex ecommerce">
                 <NavbarDesktop />
 
                 <Container maxWidth="xl">
@@ -90,29 +94,150 @@ const SitesEcommerce = () => {
                     </section>
 
                     <section>
-                        <Dropdown>
-                            <MenuButton>Plataformas</MenuButton>
+                        <Dropdown onOpenChange={() => setDropdownOpen(!dropdownOpen)}>
+                            <MenuButton className={styles.dropdownAction}>
+                                {
+                                    selected === "shopify" ? 'Shopify' :
+                                    selected === "vtex-cms" ? 'VTEX CMS' : 
+                                    selected === "vtex-io" ? 'VTEX IO' : 
+                                    'Todas'
+                                }
+                                {dropdownOpen ? <FontAwesomeIcon icon={faChevronUp} /> : <FontAwesomeIcon icon={faChevronDown} />}
+                            </MenuButton>
 
-                            <Menu>
-                                <MenuItem onClick={() => handlePlatform(platforms.all)}>Todas</MenuItem>
-                                <MenuItem onClick={() => handlePlatform(platforms.shopify)}>Shopify</MenuItem>
-                                <MenuItem onClick={() => handlePlatform(platforms.vtex_cms)}>VTEX CMS</MenuItem>
-                                <MenuItem onClick={() => handlePlatform(platforms.vtex_io)}>VTEX IO</MenuItem>
+                            <Menu style={{
+                                backgroundColor: "#FFF",
+                                borderRadius: "5px",
+                                boxShadow: "0 0 0 1px rgba(68, 68, 68, .11)",
+                                lineHeight: "40px",
+                                cursor: "pointer",
+                                outline: 0,
+                                transition: "all .2s",
+                                left: "160px",
+                                top: "-43px"
+                            }}>
+                                <MenuItem 
+                                    onClick={() => {
+                                        setSelected(platforms.all);
+                                        handlePlatform(platforms.all);
+                                    }}
+                                    style={selected === platforms.all ? {
+                                        fontWeight: 700,
+                                        backgroundColor: "#f6f6f6",
+
+                                        padding: "0px 29px",
+                                        lineHeight: "40px",
+                                        cursor: "pointer",
+                                        textAlign: "left",
+                                        fontSize: "16px"
+                                    } : {
+                                        fontWeight: 400,
+
+                                        padding: "0px 29px",
+                                        lineHeight: "40px",
+                                        cursor: "pointer",
+                                        textAlign: "left",
+                                        fontSize: "16px"
+                                    }}
+                                >Todas</MenuItem>
+
+                                <MenuItem 
+                                    onClick={() => {
+                                        setSelected(platforms.shopify);
+                                        handlePlatform(platforms.shopify);
+                                    }}
+                                    style={selected === platforms.shopify ? {
+                                        fontWeight: 700,
+                                        backgroundColor: "#f6f6f6",
+
+                                        padding: "0px 29px",
+                                        lineHeight: "40px",
+                                        cursor: "pointer",
+                                        textAlign: "left",
+                                        fontSize: "16px"
+                                    } : {
+                                        fontWeight: 400,
+
+                                        padding: "0px 29px",
+                                        lineHeight: "40px",
+                                        cursor: "pointer",
+                                        textAlign: "left",
+                                        fontSize: "16px"
+                                    }}
+                                >Shopify</MenuItem>
+
+                                <MenuItem 
+                                    onClick={() => {
+                                        setSelected(platforms.vtex_cms);
+                                        handlePlatform(platforms.vtex_cms);
+                                    }}
+                                    style={ 
+                                        selected === platforms.vtex_cms ? {
+                                            fontWeight: 700,
+                                            backgroundColor: "#f6f6f6",
+    
+                                            padding: "0px 29px",
+                                            lineHeight: "40px",
+                                            cursor: "pointer",
+                                            textAlign: "left",
+                                            fontSize: "16px"
+                                        } : {
+                                        fontWeight: 400,
+
+                                        padding: "0px 29px",
+                                        lineHeight: "40px",
+                                        cursor: "pointer",
+                                        textAlign: "left",
+                                        fontSize: "16px"
+                                    }}
+                                >VTEX CMS</MenuItem>
+
+                                <MenuItem 
+                                    onClick={() => {
+                                        setSelected(platforms.vtex_io);
+                                        handlePlatform(platforms.vtex_io);
+                                    }}
+                                    style={selected === platforms.vtex_io ? {
+                                        fontWeight: 700,
+                                        backgroundColor: "#f6f6f6",
+
+                                        padding: "0px 29px",
+                                        lineHeight: "40px",
+                                        cursor: "pointer",
+                                        textAlign: "left",
+                                        fontSize: "16px"
+                                    } : {
+                                        fontWeight: 400,
+
+                                        padding: "0px 29px",
+                                        lineHeight: "40px",
+                                        cursor: "pointer",
+                                        textAlign: "left",
+                                        fontSize: "16px"
+                                    }}
+                                >VTEX IO</MenuItem>
                             </Menu>
                         </Dropdown>
 
-                        <Box ref={galleryElement}>
+                        <Box ref={galleryElement} className={styles.storesList}>
                             {articles && 
                                 articles.map((item, index) => (
-                                    <article key={index} data-platform={item.platform}>
+                                    <article className={styles.platform} key={index} data-platform={item.platform}>
                                         <Link href={item.link} target="_blank">
+                                            <CircularProgress style={{
+                                                color: MuiTheme.palette.primary.main,
+                                                width: "45px",
+                                                height: "auto",
+                                                position: "absolute",
+                                                zIndex: 1
+                                            }} />
                                             <figure>
                                                 <Image unoptimized className={styles.siteImage} src={item.imageUrl} alt={`Representação da página inicial do(a) ${item.name}`} width={100} height={100} />
 
                                                 <figcaption>
                                                     <strong>{item.name}</strong>
 
-                                                    <Typography>Plataforma: <b>{item.platform}</b></Typography>
+                                                    <span>Plataforma: <b>{item.platform}</b></span>
 
                                                     <p>{item.description}</p>
                                                 </figcaption>
