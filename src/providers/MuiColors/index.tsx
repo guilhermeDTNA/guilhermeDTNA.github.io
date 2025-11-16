b36fbf00b79b3ad5af59c62222f5f2a9db8062ea
Guilherme <gleite98@gmail.com>
2025-11-16 17:55:35 -0300

    feat: alterando cor primária

--- src/providers/MuiColors/index.tsx
+++ src/providers/MuiColors/index.tsx
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 export const MuiTheme = createTheme({
   palette: {
     primary: {
-      main: '#5c366f',
+      main: '#2f113e',
       // light: will be calculated from palette.primary.main,
       // dark: will be calculated from palette.primary.main,
       // contrastText: will be calculated to contrast with palette.primary.main
